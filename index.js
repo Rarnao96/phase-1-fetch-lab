7cf6e147b5e85aa78885fb976407efb7f4bdd10c
Rachel Arnao <rachel.e.arnao@gmail.com>
2023-05-06 15:54:41 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(books => renderBooks(books));
 }
 
 function renderBooks(books) {
@@ -13,5 +18,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  renderBooks(fetchBooks());
 });
